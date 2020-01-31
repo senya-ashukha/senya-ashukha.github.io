c78798b3dd22cb797c0ddc9a61509d103a23ecbf
Arsenii Ashukha <ars.ashuha@gmail.com>
2020-01-31 13:38:11 +0300

    update

--- analytics.js
+++
@@ -1,9 +0,0 @@
-<!-- Global Site Tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-102973164-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-102973164-1');
-</script>
